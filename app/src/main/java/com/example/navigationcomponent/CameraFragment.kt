a8a370cc4f10f578bb7d435f4934609b178c8bd8
Anshul1507 <ag1507anshul@gmail.com>
2019-12-25 16:35:40 +0530

    Updated Communications from Fragments

--- app/src/main/java/com/example/navigationcomponent/CameraFragment.kt
+++ app/src/main/java/com/example/navigationcomponent/CameraFragment.kt
@@ -6,18 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_camera.*
+import java.util.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class CameraFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button_see_photos.setOnClickListener {
+            val random = Random()
+
+            val nextAction =
+                CameraFragmentDirections.nextAction()
+            nextAction.setNumOfPhotos(random.nextInt(100))
+
+            Navigation.findNavController(it).navigate(nextAction)
+        }
     }
 
 
--- app/src/main/java/com/example/navigationcomponent/PhotosFragment.kt
+++ app/src/main/java/com/example/navigationcomponent/PhotosFragment.kt
@@ -6,19 +6,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.navigationcomponent.Fragments.PhotosFragmentArgs
+import com.example.navigationcomponent.PhotosFragmentArgs
+import kotlinx.android.synthetic.main.fragment_photos.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class PhotosFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_photos, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_back_home.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.next_action)
+        }
+        arguments?.let {
+            val safeArgs =
+                PhotosFragmentArgs.fromBundle(it)
+            textView_num.text = "Number of Photos: ${safeArgs.numOfPhotos}"
+        }
+    }
 
 }
